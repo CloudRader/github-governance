4870edacf60397ef3bc09ccc3953e52a3c94548d
DarkRader <73800331+DarkRader@users.noreply.github.com>
2026-02-15 13:16:33 +0100

    chore: set default parameters for create repo

--- src/terraform/repository.tf
+++ src/terraform/repository.tf
@@ -1,10 +1,23 @@
 resource "github_repository" "create_repository" {
-  name        = var.repo_name
-  description = var.repo_description
+  name                        = var.repo_name
+  description                 = var.repo_description
 
-  visibility  = var.visibility
+  visibility                  = var.visibility
 
-  has_issues  = true
+  delete_branch_on_merge      = true
+  allow_merge_commit          = true
+  allow_squash_merge          = true
+  allow_rebase_merge          = true
+  allow_auto_merge            = false
+  squash_merge_commit_title   = PR_TITLE
+  squash_merge_commit_message = PR_BODY
+  merge_commit_title          = PR_TITLE
+  merge_commit_message        = PR_BODY
+
+  archive_on_destroy          = true
+
+  has_issues                  = true
+  has_wiki                    = false
 
   template {
     owner      = var.github_owner
